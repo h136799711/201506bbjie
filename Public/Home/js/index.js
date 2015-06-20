b80b26b707416c529084e9c8b31734e132cf741d
an <an@an-PC>
2015-06-20 17:48:07 +0800

    帮助中心等

--- Public/Home/js/index.js
+++ Public/Home/js/index.js
@@ -37,7 +37,45 @@ $(function(){
 		timeLunbo=window.setInterval('changeBgColor()',3000);
 	});
 	
+	
+	$("#div_main .div_info .div_bzzx .div_bzzx_zzfw li").mouseover(function(){
+		$(this).addClass("li_bzzx_over");
+	});
+	
+	$("#div_main .div_info .div_bzzx .div_bzzx_zzfw li").mouseout(function(){
+		$(this).removeClass("li_bzzx_over");
+	});
 		
+		//alert($("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc a"));
+		
+	$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc font").mouseover(function(){
+		var m=$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc font").index($(this));
+		for(var i=1; i<=m;i++){
+			$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc font").eq(i).addClass("font_bzjlc_hdzb");
+		
+		}
+		for(var i=3; i>m;i--){
+			$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc font").eq(i).removeClass("font_bzjlc_hdzb");
+		}
+		$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc_info div.div_bzzx_bbjlc_info_min").eq(m-1).show().siblings().hide();
+	})
+	
+	$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_hdgc font").mouseover(function(){
+		var m=$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_hdgc font").index($(this));
+		for(var i=1; i<=m;i++){
+			$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_hdgc font").eq(i).addClass("font_bzjlc_hdgc");
+		
+		}
+		for(var i=5; i>m;i--){
+			$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_hdgc font").eq(i).removeClass("font_bzjlc_hdgc");
+		}
+		$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_hdgc_info div.div_bzzx_hdgc_info_min").eq(m-1).show().siblings().hide();
+	})
+	
+	
+	/*$("#div_main .div_info .div_bzzx .div_bzzx_zzfw .div_bzzx_bbjlc font").mouseout(function(){
+		$(this).removeClass("font_bzjlc_hdzb");
+	})*/
 	
 })
 
